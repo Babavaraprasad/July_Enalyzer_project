f293b8f3ff57dcebdbdfbae30e0290a73b9a8388
Babavaraprasad <bapi@itu.dk>
2023-07-11 18:15:50 +0200

    Added a negative unit test sceanrio

--- july_enalyzer_proj/src/components/distribution/Distribution.test.js
+++ july_enalyzer_proj/src/components/distribution/Distribution.test.js
@@ -1,7 +1,7 @@
 import { generateNotesAndCoins } from "./Distribution";
-
+//unit test case 1---positive scenario
 describe("notes and coins test", () => {
-  test("if number of notes and coins are correct", () => {
+  test("check if number of notes and coins are correct", () => {
     const result = generateNotesAndCoins(2222);
     const expected = {
       notes: {
@@ -16,4 +16,26 @@ describe("notes and coins test", () => {
     };
     expect(result).toStrictEqual(expected);
   });
+});
+
+//unit test case 2--negative scenario
+describe("notes and coins test",()=>{
+test("check count of notes and coins are correct",()=>{
+ const result2= generateNotesAndCoins(578);
+ const expected2={
+  notes:{
+       500: 1,
+  },
+  bigCoins:{
+    50: 1,
+    20: 1,
+    5:1,
+    2:1,
+  },
+  smallCoins:{
+    1: 1
+  },
+ };
+ expect(result2).toStrictEqual(expected2);
+}) 
 });
