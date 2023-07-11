eedbe722dc2d9724bf3e10c899adf1212b4715de
Babavaraprasad <bapi@itu.dk>
2023-07-11 17:25:07 +0200

    Added a unit testing scenario to verify the distribution amount

--- july_enalyzer_proj/src/components/distribution/Distribution.test.js
+++ july_enalyzer_proj/src/components/distribution/Distribution.test.js
@@ -1,22 +1,19 @@
-import {generateNotesAndCoins} from "./Distribution";
+import { generateNotesAndCoins } from "./Distribution";
 
-describe('notes and coins test', () => {
-    test('if number of notes and coins are correct', () => {
-      const result = generateNotesAndCoins(2222);
-      const expected= {
-        notes: {
-          1000 : 2,
-          200: 1,
-        },
-        bigCoins:  {
-          20: 1,
-          2: 1
-        },
-        smallCoins: {}
-      }
-      expect(result).toStrictEqual(expected);
-    });
+describe("notes and coins test", () => {
+  test("if number of notes and coins are correct", () => {
+    const result = generateNotesAndCoins(2222);
+    const expected = {
+      notes: {
+        1000: 2,
+        200: 1,
+      },
+      bigCoins: {
+        20: 1,
+        2: 1,
+      },
+      smallCoins: {},
+    };
+    expect(result).toStrictEqual(expected);
+  });
 });
-
-
-
